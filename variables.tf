fa45da4d400beb6769947645f7c9fdc0de4b5b8c
abdulhexa <121549928+abdulhexa@users.noreply.github.com>
2023-01-19 17:48:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,9 @@
 
-variable "location" {
-  type        = string
-  description = "Azure region to use. Defaults to East US."
-  default     = "eastus"
-}
 
 variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
-  default = "demo1"
+  description = "A prefix used for all resources in this example"
 }
 
-
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be provisioned"
+}
