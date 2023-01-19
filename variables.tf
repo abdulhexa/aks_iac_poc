66c2b17a688c1e220296c9fd05af8562005b7d1c
abdulhexa <121549928+abdulhexa@users.noreply.github.com>
2023-01-19 15:17:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
-variable "resource_group_name" {
-  type        = string
-  description = "Name of resource group to create. Defaults to oidc-test."
-  default     = "oidc-simple-rg-feature"
-}
 
 variable "location" {
   type        = string
   description = "Azure region to use. Defaults to East US."
   default     = "eastus"
+}
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
 }
