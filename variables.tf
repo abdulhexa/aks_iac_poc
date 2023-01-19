5fe88fbce0b2d906676660a39bbffc6a5455dd37
abdulhexa <121549928+abdulhexa@users.noreply.github.com>
2023-01-19 15:38:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,6 +9,4 @@ variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
 }
 
-variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
-}
+
